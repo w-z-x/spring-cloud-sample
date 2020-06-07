bd20eac199d42ebe06effb5a8702c5aeedb84f24
zhxwu <zhxwu@hotmail.com>
2020-06-07 11:07:57 +0800

    不再支持将zipkin-server作为依赖项添加到spring-boot程序

---
+++ spring-cloud-zipkin/README.md
@@ -0,0 +1,4 @@
+#spring-cloud-zipkin
+
+##Custom servers are not supported
+https://github.com/openzipkin/zipkin/tree/master/zipkin-server
---
+++ spring-cloud-zipkin/src/main/java/com/azurer/spring/zipkin/ZipkinServer.java
@@ -0,0 +1,12 @@
+package com.azurer.spring.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServer.class, args);
+    }
+
+}
